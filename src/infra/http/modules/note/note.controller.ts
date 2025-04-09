0278b90da844dbcd66e24a3dba1ca1907cbe45c8
Rian Xavier <rianalvesxvr@gmail.com>
2025-04-09 10:49:20 -0300

    feat: route to delete note, done

--- src/infra/http/modules/note/note.controller.ts
+++ src/infra/http/modules/note/note.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Param, Post, Put, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  Put,
+  Request,
+} from '@nestjs/common';
 import { CreateNoteUseCase } from 'src/modules/note/useCases/createNoteUseCase/createNoteUseCase';
 import { AuthenticatedRequestModel } from '../auth/models/authenticatedRequestModel';
 import { CreateNoteBody } from './dtos/CreateNoteBody';
 import { NoteViewModel } from './viewModel/NoteViewModel';
 import { EditNoteUseCase } from 'src/modules/note/useCases/editNoteUseCase/editNoteUseCase';
 import { EditNoteBody } from './dtos/EditNoteBody';
+import { DeleteNoteUseCase } from 'src/modules/note/useCases/deleteNoteUseCase/deleteNoteUseCase';
 
 @Controller('notes')
 export class NoteController {
   constructor(
     private createNoteUseCase: CreateNoteUseCase,
     private editNoteUseCase: EditNoteUseCase,
+    private deleteNoteUseCase: DeleteNoteUseCase,
   ) {}
 
   @Post()
@@ -42,6 +52,17 @@ export class NoteController {
       title,
       userId: request.user.id,
       description,
+    });
+  }
+
+  @Delete(':id')
+  async deleteNote(
+    @Request() request: AuthenticatedRequestModel,
+    @Param('id') noteId: string,
+  ) {
+    await this.deleteNoteUseCase.execute({
+      noteId,
+      userId: request.user.id,
     });
   }
 }
